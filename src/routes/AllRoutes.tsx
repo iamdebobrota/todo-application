54937c19132c660edcdb92bd5d5861208a3c5b7f
iamdebobrota <iamdebobrota@gmail.com>
2024-07-28 16:52:38 +0530

    complete: todo - get, add and delete features

--- src/routes/AllRoutes.tsx
+++ src/routes/AllRoutes.tsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import NotFoundPage from '../pages/NotFoundPage'
-import Navbar from '../components/Navbar'
-import Todo from '../pages/todo/Todo'
-import Home from '../pages/Home'
+import { Route, Routes } from "react-router-dom";
+import NotFoundPage from "../pages/NotFoundPage";
+import Navbar from "../components/Navbar";
+import Todo from "../pages/todo/Todo";
+import Home from "../pages/Home";
 
 const AllRoutes = () => {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/todo' element={<Todo />}/>
-        <Route path='*' element={<NotFoundPage />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/todo" element={<Todo />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default AllRoutes
+export default AllRoutes;
