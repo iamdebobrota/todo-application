99f0b7677efb91b06a731f162fd3cc40ebfa676b
iamdebobrota <iamdebobrota@gmail.com>
2024-08-12 17:13:03 +0530

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,27 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [auth, setAuth] = useState(false);
+
+const handleClick=()=>{
+setAuth((prev)=> !prev);
+}  
   return (
     <div
       style={{
         marginTop: "25px",
         display: "flex",
         alignItems: "center",
+        flexDirection: "column",
+        gap: "20px",
         justifyContent: "center",
         height: "80vh",
       }}>
       <h1>Home</h1>
+      {auth && <h2>You are logged in as a Admin</h2>}
+      <button onClick={handleClick}>{auth ? "Logout" : "Login as a Admin"}</button>
     </div>
   );
 };
-
 export default Home;
+
